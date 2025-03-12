8f896c795792386b95972e8ec4d5f4bde2b9bba5
Aman Bhondge <amanbhondge@gmail.com>
2025-03-12 13:19:52 +0530

    6

--- src/index.js
+++ src/index.js
@@ -14,7 +14,10 @@ import userRouter from './routes/user.route.js';
 const app = express();
 const server = http.createServer(app);
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
